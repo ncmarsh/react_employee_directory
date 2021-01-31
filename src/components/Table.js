150991375ebf7a06c4719038798f130f37e762cb
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-01-30 18:21:36 -0800

    added hover class to name table header

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,7 +7,7 @@ function Table(props) {
         <thead>
             <tr>
             <th scope="col">Picture</th>
-            <th value="name.first" scope="col" className="sort" onClick={props.colOrder}>Name</th>
+            <th value="name.first" scope="col" className="sort-option" onClick={props.colOrder}>Name</th>
             <th value="phone" scope="col" onClick={props.colOrder}>Phone</th>
             <th value="email" scope="col" onClick={props.colOrder}>Email</th>
             <th value="dob" scope="col" onClick={props.colOrder}>DOB</th>
