b799504a4c3b4f589fc658c76119c04a517448f8
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-01-30 20:57:18 -0800

    removing console logs

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,9 +3,6 @@ const BASEURL = "https://randomuser.me/api/";
 
 const API = {
     search: function(query) {
-        console.log(query);
-        console.log(BASEURL + query);
-        console.log(axios.get(BASEURL + query));
         return axios.get(BASEURL + query);
     }
 };
